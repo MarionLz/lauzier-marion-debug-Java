b4e2abec08d237830e2fccce8e09552ab0d4e894
Cy\Cy <marion.lauzier.ad@gmail.com>
2025-01-27 18:16:04 +0100

    refactorisation of AnalyticsCounter

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,13 +3,46 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
+	private static ISymptomReader reader;
+	private static ISymptomWriter writer;
+	
+	public AnalyticsCounter (ISymptomReader reader, ISymptomWriter writer) { 
+		this.reader = reader;
+		this.writer = writer;
+	}
+	
+	public List<String> getSymptoms() { 
+		return this.reader.GetSymptoms();
+	}
+	
+	public Map<String, Integer> countSymptoms(List<String> symptoms) { 
+		Map<String, Integer> listSymptomsAndCount = new TreeMap<String, Integer>();
+		for(String symptom : symptoms) {
+			listSymptomsAndCount.merge(symptom, 1, Integer::sum);
+		}
+		return listSymptomsAndCount;
+	}
+	
+	public Map<String, Integer> sortSymptoms(Map<String, Integer> symptoms) { 
+		Map<String, Integer> sortedsymptoms = new TreeMap<String, Integer>(symptoms);
+		return  sortedsymptoms;
+	}
+	
+	public void writeSymptoms(Map<String, Integer> symptoms) { 
+		this.writer.writeSymptoms(symptoms);
+	}
 	
 	public static void main(String args[]) throws Exception {
+		
+		/*
 		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
@@ -38,6 +71,6 @@ public class AnalyticsCounter {
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		writer.close();*/
 	}
 }
